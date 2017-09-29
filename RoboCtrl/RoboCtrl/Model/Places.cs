a3594bd188509bdf069239fd834a8d72953c0af6
Andor Schindler <schindler.andor@nexogen.hu>
2017-09-29 10:48:35 +0200

    ads

---
+++ RoboCtrl/RoboCtrl/Model/Places.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboCtrl.Model
+{
+    class Place
+    {
+        
+    }
+
+    class Obtacle: Place
+    {
+    }
+
+    class Box: Place
+    {
+
+    }
+
+    class Robot: Place
+    {
+
+    }
+}
---
+++ RoboCtrl/RoboCtrl/Model/Warehouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboCtrl.Model
+{
+    class Warehouse
+    {
+        public readonly Place[,] places;
+        public readonly int width;
+        public readonly int heigth;
+
+        public Warehouse(int w, int h)
+        {
+            this.width = w;
+            this.heigth = h;
+
+            Warehouse = new Place[w, h];
+        }
+    }
+}
