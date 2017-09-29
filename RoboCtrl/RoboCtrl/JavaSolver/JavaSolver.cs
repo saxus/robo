9a196d85472a11adf6b82707ecbc873d0d874b19
Andor Schindler <schindler.andor@nexogen.hu>
2017-09-29 13:17:22 +0200

    Solve interface

---
+++ RoboCtrl/RoboCtrl/JavaSolver/JavaSolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoboCtrl.Model;
+
+namespace RoboCtrl.JavaSolver
+{
+    class JavaSolver
+    {
+        public IEnumerable<Move> Solve(string map)
+        {
+            return new Move[0];
+        }
+    }
+}
